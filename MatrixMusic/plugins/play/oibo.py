8cd13730ae79703b8f3b266d321a324f756ed64e
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-15 22:31:01 +0200

    Update oibo.py

--- MatrixMusic/plugins/play/oibo.py
+++ MatrixMusic/plugins/play/oibo.py
@@ -13,7 +13,7 @@ from MatrixMusic import app
 
 
 ########################### بوت حذف
-@app.on_message(filters.command(["حذف", "عاوز احذف", "عاوزه احذف"], ""))
+@app.on_message(filters.command(["الحذف", "عاوز احذف", "عاوزه احذف"], ""))
 async def svksksa(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph//file/a6137caa707bdb1247d7c.jpg",
