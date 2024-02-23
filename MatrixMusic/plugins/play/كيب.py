7ef0d598b1e1b3f9112c71e8261fee48d91f058e
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-23 12:10:04 +0200

    Update كيب.py

--- MatrixMusic/plugins/play/كيب.py
+++ MatrixMusic/plugins/play/كيب.py
@@ -1,6 +1,5 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
 from strings.filters import command
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
