de88f8c5d78e14d7b9fa3fe02e8975a9232d5a42
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-24 15:32:01 +0200

    Update كت تويت.py

--- MatrixMusic/plugins/play/كت تويت.py
+++ MatrixMusic/plugins/play/كت تويت.py
@@ -3715,7 +3715,7 @@ iddof = []
 )
 async def iddlock(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in ["creator", "administrator"]:
+   if get.status in ["ChatMemberStatus.ADMINISTRATOR"]:
       if message.chat.id in iddof:
         return await message.reply_text("تم معطل من قبل√")
       iddof.append(message.chat.id)
@@ -3730,7 +3730,7 @@ async def iddlock(client, message):
 )
 async def iddopen(client, message):
    get = await app.get_chat_member(message.chat.id, message.from_user.id)
-   if get.status in ["creator", "administrator"]:
+   if get.status in ["ChatMemberStatus.ADMINISTRATOR"]:
       if not message.chat.id in iddof:
         return await message.reply_text("كت مفعل من قبل √")
       iddof.remove(message.chat.id)
