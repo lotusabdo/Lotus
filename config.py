69a119d960f5f3eda751967b042220038dc6495b
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-25 08:19:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ super_sudoers = [6456857472]
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", 17154185))
-API_HASH = getenv("API_HASH", "7d24797dd1d72adbe8ee2596be2904d0")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://proceed58:proceed58@cluster0.p5s9ym5.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 2000))
 
