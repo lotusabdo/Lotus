ba6727776ffc193b7f42ebce9f191b6119d6c9bd
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-26 20:21:20 +0200

    Delete MatrixMusic/plugins/play/tasfyaWaMoshref.py

--- MatrixMusic/plugins/play/tasfyaWaMoshref.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import ChatPermissions #ChatPrivileges
-import asyncio, requests 
-from MatrixMusic import app
-#by > @PROGRAMMER_TOM / @BENN_DEV
-
