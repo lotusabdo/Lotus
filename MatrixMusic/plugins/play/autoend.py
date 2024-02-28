b5e2c8dcbb05651948e1f32b8c063e513f9b8836
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-02-28 22:34:44 +0200

    Update autoend.py

--- MatrixMusic/plugins/play/autoend.py
+++ MatrixMusic/plugins/play/autoend.py
@@ -7,9 +7,6 @@ from MatrixMusic.misc import SUDOERS
 from MatrixMusic.utils.database import autoend_off, autoend_on
 from MatrixMusic.utils.decorators.language import language
 
-# Commands
-AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
-
 
 @app.on_message(filters.command(AUTOEND_COMMAND) & SUDOERS)
 async def auto_end_stream(client, message):
