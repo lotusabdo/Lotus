092e6e36a22db71eda514f28b5499a156832a6c3
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-03-17 09:10:05 +0200

    Update start.py

--- MatrixMusic/utils/inline/start.py
+++ MatrixMusic/utils/inline/start.py
@@ -9,7 +9,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text="اضغط لاضافتي لمجموعتك✅", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text="𝙶𝚁𝙾̀𝚄𝙿", url= "https://t.me/jx_xll"),
+            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url= "https://t.me/jx_xll"),
         ],
     ]
     return buttons
@@ -25,12 +25,12 @@ def private_panel(_):
         ],
         
         [
-            InlineKeyboardButton(text="مطور البوت", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="ᏀᎡΌႮᏢ", url=f"https://t.me/jx_xll"), 
+            InlineKeyboardButton(text="مطور السورس ", url=f"https://t.me/EU_TM"),
+            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url=f"https://t.me/jx_xll"), 
         ],
         [
             
-            InlineKeyboardButton(text="ՏΌႮᎡᏟᎬ ᏞΌͲႮՏ", url=f"https://t.me/l2_2Y") , 
+            InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=f"https://t.me/l2_2Y") , 
         ],
     ]
     return buttons
